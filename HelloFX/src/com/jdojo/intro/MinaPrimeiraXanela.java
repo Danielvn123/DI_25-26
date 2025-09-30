f2d87c6d7d5f73ed20a07fef934b004898e03260
Danielvn123 <daniel.vilas03@gmail.com>
2025-09-30 12:07:28 +0200

    Primer commit

---
+++ HelloFX/src/com/jdojo/intro/MinaPrimeiraXanela.java
@@ -0,0 +1,37 @@
+package com.jdojo.intro;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MinaPrimeiraXanela extends Application {
+	
+		public static void main(String[] args) {
+			Application.launch(args);
+		}
+
+		@Override
+		public void start(Stage stage) throws Exception {
+			VBox group = new VBox();
+			
+			Button btn = new Button ("Cambiar titulo");
+
+			btn.setOnAction(e -> {
+	            if (stage.getTitle().equals("Miña primeira xanela")) {
+	            	stage.setTitle("Titulo cambiado");
+	            } else {
+	            	stage.setTitle("Miña primeira xanela");
+	            }
+	        });
+			
+			group.getChildren().add(btn);
+
+			Scene scene = new Scene(group, 400, 300);
+			stage.setScene(scene);
+			stage.setTitle("Miña primeira xanela");
+			stage.show();
+		}
+
+	}
