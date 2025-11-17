b21e012aff9a2cbd6ae0da437d05e22a3add6231
Danielvn123 <daniel.vilas03@gmail.com>
2025-11-17 10:21:33 +0100

    17/11/2025

--- HelloFX/src/com/jdojo/exercicios/Exercicio13_StudentTest.java
+++ HelloFX/src/com/jdojo/exercicios/Exercicio13_StudentTest.java
@@ -34,3 +34,47 @@ public class Exercicio13_StudentTest {
 		System.out.println(", Value:" + value + "]");
 	}
 }
+/*
+
+import javafx.beans.property.ReadOnlyProperty;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student("S000", "Descoñecido", 0.0);
+
+		// Imprimimos o valor inicial das propiedades
+		System.out.println("------Valores iniciais------");
+		printDetails(student.idProperty());
+		printDetails(student.nameProperty());
+		printDetails(student.averageGradeProperty());
+
+		// Cambiamos o nome a idade e a nota
+		student.nameProperty().set("Ana");
+		student.averageGradeProperty().set(10.0);
+		// Probamos que mediante as propiedades JavaFx o id non se pode modificar dende
+		// fora
+		// student.idProperty().set("S001");
+
+		// Imprimimos o valor cambiado das propiedades
+		System.out.println("------Valores cambiados------");
+		printDetails(student.idProperty());
+		printDetails(student.nameProperty());
+		printDetails(student.averageGradeProperty());
+
+	}
+
+	// Método que imprime detalles dunha propiedade
+	public static void printDetails(ReadOnlyProperty<?> property) {
+		String name = property.getName();
+		Object value = property.getValue();
+		Object bean = property.getBean();
+		String beanClassName = (bean == null) ? "null" : bean.getClass().getSimpleName();
+		String propClassName = property.getClass().getSimpleName();
+
+		System.out.println(propClassName);
+		System.out.print("[Nome da propiedade:" + name);
+		System.out.print(", Bean Class:" + beanClassName);
+		System.out.println(", Valor da propiedade:" + value + "]");
+	}
+}
+*/
--- HelloFX/src/com/jdojo/exercicios/Exercicio15.java
+++ HelloFX/src/com/jdojo/exercicios/Exercicio15.java
@@ -36,4 +36,43 @@ public class Exercicio15 extends Application{
 		Application.launch(args);
 	}
 }
+/*import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CocheFxApp extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        Coche meuCoche = new Coche();
+        meuCoche.setMarca("McLaren");
+        meuCoche.setVelocidade(100);
+        
+        Label marcaLabel = new Label();
+        Label velocidadeLabel = new Label();
+        
+        // VINCULACIÓN: o texto dos labels está ligado ás propiedades do coche.
+        marcaLabel.textProperty().bind(meuCoche.marcaProperty());
+        velocidadeLabel.textProperty().bind(meuCoche.velocidadeProperty().asString("Velocidade: %d km/h"));
+        
+        Button botonAcelerar = new Button("Acelerar (+25 km/h)");
+        botonAcelerar.setOnAction(event -> meuCoche.acelerar(25));
+        
+        VBox root = new VBox(15, marcaLabel, velocidadeLabel, botonAcelerar);
+        root.setAlignment(Pos.CENTER);
+        
+        Scene scene = new Scene(root, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("Coche Dinámico");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}*/
 
