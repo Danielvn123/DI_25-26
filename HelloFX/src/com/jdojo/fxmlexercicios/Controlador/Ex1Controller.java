27a887bd6a59ed406126956b6c4241957fbba4f7
Danielvn123 <daniel.vilas03@gamil.com>
2026-01-13 13:48:06 +0100

    13/01

---
+++ HelloFX/src/com/jdojo/fxmlexercicios/Controlador/Ex1Controller.java
@@ -0,0 +1,46 @@
+package com.jdojo.fxmlexercicios.Controlador;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Ex1Controller implements Initializable{
+
+    @FXML
+    private Button Boton1;
+
+    @FXML
+    private Label Label1;
+
+    @FXML
+    private Label Label2;
+
+    @FXML
+    private Label Label3;
+
+    @FXML
+    private TextField TextField1;
+
+    @FXML
+    private TextField TextField2;
+
+    @FXML
+    private TextField TextField3;
+
+    @FXML
+    void sumar(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
